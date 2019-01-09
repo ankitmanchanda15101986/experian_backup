a8877ae0d0eaa60e63290220c3ccc935466bc55a
Ankit Manchanda <manchanda.a@HCL.COM>
2019-01-09 20:36:04 +0530

    Chatbot response updated

--- src/main/java/com/experian/dto/chatbot/response/ChatBotScore.java
+++ src/main/java/com/experian/dto/chatbot/response/ChatBotScore.java
@@ -9,44 +9,45 @@ package com.experian.dto.chatbot.response;
  */
 public class ChatBotScore {
 
-	private String Category;
-	private String Word;
-	private Integer Count;
-	
+	private String category;
+	private String word;
+	private Integer cnt;
 	/**
 	 * @return the category
 	 */
 	public String getCategory() {
-		return Category;
+		return category;
 	}
 	/**
 	 * @param category the category to set
 	 */
 	public void setCategory(String category) {
-		Category = category;
+		this.category = category;
 	}
 	/**
 	 * @return the word
 	 */
 	public String getWord() {
-		return Word;
+		return word;
 	}
 	/**
 	 * @param word the word to set
 	 */
 	public void setWord(String word) {
-		Word = word;
+		this.word = word;
 	}
 	/**
-	 * @return the count
+	 * @return the cnt
 	 */
-	public Integer getCount() {
-		return Count;
+	public Integer getCnt() {
+		return cnt;
 	}
 	/**
-	 * @param count the count to set
+	 * @param cnt the cnt to set
 	 */
-	public void setCount(Integer count) {
-		Count = count;
-	}	
+	public void setCnt(Integer cnt) {
+		this.cnt = cnt;
+	}
+	
+	
 }
